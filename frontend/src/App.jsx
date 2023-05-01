0a4fd79a0eefaddec5126689410fdbba0f4a2e19
minkman <lomick2090@gmail.com>
2023-05-01 15:43:13 -0500

    Add contracts to App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,23 @@
 import React, {useState, useEffect} from 'react'
 import {ethers } from 'ethers'
 import Header from './components/Header'
+import farmABI from './ABIs/farmABI.json'
+import FTKABI from './ABIs/FTKABI.json'
 
 export default function App() {
     const [provider, setProvider] = useState(new ethers.providers.Web3Provider(window.ethereum));
     const [walletInfo, setWalletInfo] = useState({});
+    const [FTKContract, setFTKContract] = useState();
+    const [FarmContract, setFarmContract] = useState();
+
+    async function initialize() {
+        const FTKAddy = '0x9e414bd721a1e8Cca621bf996A8Fd3c200a7Aa88';
+        const FarmAddy = '0x706Bea3A304A771ed759122C9030D2bF28085032';
+
+        setFTKContract(new ethers.Contract(FTKAddy, FTKABI, provider))
+        setFarmContract(new ethers.Contract(FarmAddy, farmABI, provider))
+
+    }
 
 
     async function connectWallet() {
